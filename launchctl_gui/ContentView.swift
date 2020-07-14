7427d347ec495c76ac553abcb0d9b277c00dc87e
Reid <reidpritchard99@gmail.com>
2020-07-14 10:17:55 -0600

    Initial Commit

---
+++ launchctl_gui/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  launchctl_gui
+//
+//  Created by Reid Pritchard on 7/14/20.
+//  Copyright © 2020 Reid Pritchard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
